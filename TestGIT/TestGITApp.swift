bffc9e744fd26b73ba2cb571c27fc09582bfb29e
John Anderson <inbox@johnanderson.co.nz>
2023-12-22 07:04:04 +1300

    Initial Commit

---
+++ TestGIT/TestGITApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGITApp.swift
+//  TestGIT
+//
+//  Created by John Anderson on 22/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TestGITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
