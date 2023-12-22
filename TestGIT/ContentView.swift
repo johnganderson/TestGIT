650b716cd26494df001fcc869953d46c2035364e
John G Anderson <johnganderson@users.noreply.github.com>
2023-12-22 19:54:56 +1300

    Update ContentView.swift
    Changed at GITHub

--- TestGIT/ContentView.swift
+++ TestGIT/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, everyone at GIT!")
+            Text("Hello, everyone at GITHUB changed me!")
         }
         .padding()
     }
