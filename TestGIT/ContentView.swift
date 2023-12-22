48980f5ffdaa493c3618f397a5e866c0839932a1
John Anderson <inbox@johnanderson.co.nz>
2023-12-22 20:04:07 +1300

    Isay goodbye

--- TestGIT/ContentView.swift
+++ TestGIT/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, everyone at GITHUB changed me - and I let them!")
+            Text("Goodbye, everyone !")
         }
         .padding()
     }
