0cce96cd3b9ea0125146ffd2fcf10182ac746d36
John Anderson <inbox@johnanderson.co.nz>
2023-12-22 20:01:53 +1300

    i let them

--- TestGIT/ContentView.swift
+++ TestGIT/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, everyone at GITHUB changed me!")
+            Text("Hello, everyone at GITHUB changed me - and I let them!")
         }
         .padding()
     }
